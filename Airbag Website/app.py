d48781ac75c20e0ee94698035aede7cf77cf5081
jasonli35 <lijaosn@gmail.com>
2023-06-08 01:52:08 -0700

    added slider and mqtt

--- Airbag Website/app.py
+++ Airbag Website/app.py
@@ -332,7 +332,31 @@ def get_sessions(request:Request) -> dict:
 
 @app.post("/slider-update")
 def slider_update(slider: SliderValue):
-  print(slider.val)
+  client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+  # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+  # userdata is user defined data of any type, updated by user_data_set()
+  # client_id is the given name of the client
+  client.on_connect = mqttComms.on_sub_connect
+
+
+  # enable TLS for secure connection
+  client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+  # set username and password
+  username = "airbagjacket"
+  password = "a11n3wp4nt5"
+  url = "461c2f8dac1642c9b29ae68be3d90e2d.s2.eu.hivemq.cloud"
+  client.username_pw_set(username, password)
+  # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+  client.connect(url, 8883)
+
+  # setting callbacks, use separate functions like above for better visibility
+  client.on_subscribe = mqttComms.on_subscribe
+  client.on_message = on_message
+  client.on_publish = mqttComms.on_publish
+  client.loop_start()
+  client.public("airbag/slider", slider.val, qos=1)
+  client.loop_stop()
+  # print(slider.val)
 
 
 if __name__ == "__main__":
